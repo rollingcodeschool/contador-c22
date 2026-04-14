e81edb9db713707528615f617fc3643afd7c60e4
Emilse Arias <emilsearias@gmail.com>
2026-04-13 21:18:02 -0300

    agregamos la logica del componente contador, incorporando un state que incrementa y decrementa el contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 const Contador = () => {
+// const [nombreState, nombreFuncion que actualiza al state] = useState(valor inicial)
+const [numero, setNumero] = useState(0)
+// let numero = 0
+
+// const incrementar = ()=>{
+//     //numero++ // numero = numero +1
+//     setNumero(numero + 1)
+// }
+
     return (
         <section className="border rounded-3 text-center p-3">
             <h2>Contador</h2>
-            <p className="lead">1</p>
-            <button className="btn btn-primary me-3">+</button>
-            <button className="btn btn-danger">-</button>
+            <p className="lead">{numero}</p>
+            <button className="btn btn-primary me-3" onClick={()=>setNumero(numero + 1) }>+</button>
+            <button className="btn btn-danger" onClick={()=> setNumero(numero - 1)}>-</button>
         </section>
     );
 };
